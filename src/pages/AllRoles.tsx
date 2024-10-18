6dac32ba94c26c7c3064154861537d490221ca0b
Bernard Mattéo <matteo_bernard1@ens.univ-artois.fr>
2024-10-18 23:27:52 +0200

    Fix all roles display

--- src/pages/AllRoles.tsx
+++ src/pages/AllRoles.tsx
@@ -3,9 +3,9 @@ import { Role } from "../types";
 import { useState } from "react";
 import { transformRoleId } from "../utils";
 import defaultImage from '../assets/img/wv-logo-nobg.png';
-import {RoleDialog} from "../component";
-import {RootState} from "../redux";
-import {useSelector} from "react-redux";
+import { RoleDialog } from "../component";
+import { RootState } from "../redux";
+import { useSelector } from "react-redux";
 
 export const AllRoles = () => {
     const [selectedRole, setSelectedRole] = useState<Role | null>(null);
@@ -17,21 +17,23 @@ export const AllRoles = () => {
                 <h1 className="text-4xl font-bold mb-6 text-center">Roles</h1>
                 <div className="grid grid-cols-4 gap-4">
                     {roles.data ? (
-                        roles.data.roles.map((role) => (
-                            <div
-                                key={role.id}
-                                className="flex flex-col items-center p-4 border border-2 rounded-lg bg-wv-white cursor-pointer"
-                                onClick={() => setSelectedRole(role)}
-                            >
-                                <img
-                                    src={role.image.url || defaultImage}
-                                    alt={role.id}
-                                    className="w-16 h-16 mb-2"
-                                    onError={(e) => (e.currentTarget.src = defaultImage)}
-                                />
-                                <h2 className="text-lg font-bold text-center">{transformRoleId(role.id)}</h2>
-                            </div>
-                        ))
+                        roles.data.roles
+                            .filter((role) => !role.id.includes('random'))
+                            .map((role) => (
+                                <div
+                                    key={role.id}
+                                    className="flex flex-col items-center p-4 border border-2 rounded-lg bg-wv-white cursor-pointer"
+                                    onClick={() => setSelectedRole(role)}
+                                >
+                                    <img
+                                        src={role.image.url || defaultImage}
+                                        alt={role.id}
+                                        className="w-16 h-16 mb-2"
+                                        onError={(e) => (e.currentTarget.src = defaultImage)}
+                                    />
+                                    <h2 className="text-lg font-bold text-center">{transformRoleId(role.id)}</h2>
+                                </div>
+                            ))
                     ) : (
                         <p className="text-lg">Loading...</p>
                     )}
